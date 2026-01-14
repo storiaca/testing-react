8957a13ebc55db5437c162b6729fd504716dd240
Aleksandar Ristic <storiaca@gmail.com>
2026-01-14 10:09:12 +0100

    Test for errors

--- project-vitest/src/App.tsx
+++ project-vitest/src/App.tsx
@@ -1,7 +1,14 @@
 import UseArrayExample from "./components/Hooks/UseArrayExample";
+import { ShoppingList1 } from "./components/Shopping/ShoppingList1";
+import { ShoppingList2 } from "./components/Shopping/ShoppingList2";
 import { SimplePost } from "./components/SimplePost";
 
 function App() {
+  const ingredients = ['Apples', 'Bananas', 'Ham', 'Bread', 'Bread']
+
+  const someFunction = (selectedItem: string) => {
+    console.log(`Selected ${selectedItem}`);
+  }
 
   return (
     <>
@@ -11,6 +18,12 @@ function App() {
         likesBy={['John','Mary']}
       />
       <UseArrayExample />
+
+      <h2>Shopping list 1:</h2>
+      {/* <ShoppingList1 groceries={ingredients} selectItem={someFunction}/> */}
+
+      <h2>Shopping list 2:</h2>
+      <ShoppingList2 groceries={ingredients} selectItem={someFunction}/>
     </>
   )
 }
