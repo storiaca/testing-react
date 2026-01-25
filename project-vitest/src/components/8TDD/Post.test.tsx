ffad8a289ef9904f7724d149ad74054714852e59
Aleksandar Ristic <storiaca@gmail.com>
2026-01-25 17:47:14 +0100

    TDD start

---
+++ project-vitest/src/components/8TDD/Post.test.tsx
@@ -0,0 +1,16 @@
+import { Post } from "./Post"
+import { render, screen, within, act } from "@testing-library/react"
+import * as DataService from  './DataService'
+import userEvent from "@testing-library/user-event"
+
+describe('Post tests with mocks', () => {
+  it('should load initial comments',async () => {
+
+  })
+
+
+  it('should invoke backend when posting comment', async () => {
+        
+
+  })
+})
